03b7cb7fcc40d97e81fbac0f4f144fa8f2ace195
inavasi <inavas@outlook.de>
2024-02-22 16:14:02 +0100

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals.js";
 
-ReactDOM.createRoot(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
